7177bdbbb5f8b7565394d6a0fdb8d6f5777ee58b
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-01-24 01:06:26 -0300

    Estilos globais

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import { ThemeProvider } from 'styled-components';
+import theme from './src/global/styles/theme';
 import { Home } from './src/pages/Home';
 
 const App: React.FunctionComponent = () => {
-  return <Home />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
 };
 
 export default App;
