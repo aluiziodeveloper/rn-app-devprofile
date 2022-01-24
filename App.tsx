8f6e364e8a00475c7e742569908ac23706b0b447
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-01-24 00:22:58 -0300

    Pagina Home

--- App.tsx
+++ App.tsx
@@ -1,21 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { Home } from './src/pages/Home';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App: React.FunctionComponent = () => {
+  return <Home />;
+};
+
+export default App;
