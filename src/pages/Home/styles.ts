abe2d4e5d2793126b98104dd2cd76bf79b791dcb
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-01-25 13:13:42 -0300

    Header da pagina Home - parte 4

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -22,14 +22,32 @@ export const UserWrapper = styled.View`
   padding: 0 24px;
 `;
 
-export const UserInfo = styled.View``;
+export const UserInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
 
 export const UserAvatarButton = styled.TouchableOpacity``;
 
-export const UserAvatar = styled.Image``;
+export const UserAvatar = styled.Image`
+  width: ${RFValue(52)}px;
+  height: ${RFValue(52)}px;
+  border-radius: 10px;
+`;
 
-export const UserInfoDetail = styled.View``;
+export const UserInfoDetail = styled.View`
+  margin-left: 17px;
+`;
 
-export const UserGreeting = styled.Text``;
+export const UserGreeting = styled.Text`
+  color: ${({ theme }) => theme.colors.gray800};
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+`;
 
-export const UserName = styled.Text``;
+export const UserName = styled.Text`
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.bold};
+  font-weight: bold;
+  color: ${({ theme }) => theme.colors.gray800};
+`;
