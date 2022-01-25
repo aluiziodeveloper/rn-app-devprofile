410f41326bb1ca3f4a56a7483a1150985b998960
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-01-25 12:31:28 -0300

    Header da pagina Home - parte 2

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import { Container, Header } from './styles';
 
+import avatarDefault from '../../assets/avatar02.png';
+
 export const Home: React.FunctionComponent = () => {
   return (
     <Container>
-      <Header />
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <UserAvatarButton onPress={}>
+              <UserAvatar source={avatarDefault} />
+            </UserAvatarButton>
+            <UserInfoDetail>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Jorge</UserName>
+            </UserInfoDetail>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 };
