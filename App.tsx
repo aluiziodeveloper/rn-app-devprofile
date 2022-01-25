a4b688d2e9bb0662c2c7e381e57bc651e423a227
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-01-25 11:09:50 -0300

    Instalando fonts

--- App.tsx
+++ App.tsx
@@ -2,8 +2,23 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/global/styles/theme';
 import { Home } from './src/pages/Home';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+import AppLoading from 'expo-app-loading';
 
 const App: React.FunctionComponent = () => {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Home />
