ffe6fa663f4921e6ff6632c952ed0c46802fa961
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-01-26 15:23:34 -0300

    Auth Context - parte 2

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -4,6 +4,18 @@ interface IAuthContext {
   name: string;
 }
 
+interface IProps {
+  children: React.ReactElement;
+}
+
 export const AuthContext = React.createContext<IAuthContext>(
   {} as IAuthContext,
 );
+
+export const AuthProvider: React.FunctionComponent<IProps> = ({ children }) => {
+  return (
+    <AuthContext.Provider value={{ name: 'Jorge' }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
