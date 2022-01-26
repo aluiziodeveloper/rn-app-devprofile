7dea360f33f76a03f8cdf8b0d509eef6ca5107ad
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-01-26 12:03:18 -0300

    Refatoracao da pagina SignIn

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { ScrollView, KeyboardAvoidingView, Platform, View } from 'react-native';
+import { useForm, FieldValues } from 'react-hook-form';
+
 import { useNavigation } from '@react-navigation/native';
 
 import { Button } from '../../components/Form/Button';
-import { Input } from '../../components/Form/Input';
 import {
   Container,
   Content,
@@ -16,13 +17,29 @@ import {
   Title,
 } from './styles';
 import logo from '../../assets/logo.png';
+import { InputControl } from '../../components/Form/InputControl';
 
 interface ScreenNavigationProp {
   navigate: (screen: string) => void;
 }
 
+interface IFormInputs {
+  [name: string]: any;
+}
+
 export const SignIn: React.FunctionComponent = () => {
+  const { handleSubmit, control } = useForm<FieldValues>();
+
   const { navigate } = useNavigation<ScreenNavigationProp>();
+
+  const handleSignIn = (form: IFormInputs) => {
+    const data = {
+      email: form.email,
+      password: form.password,
+    };
+
+    console.log(data);
+  };
 
   return (
     <KeyboardAvoidingView
@@ -40,10 +57,23 @@ export const SignIn: React.FunctionComponent = () => {
             <View>
               <Title>Faça seu logon</Title>
             </View>
-            <Input placeholder="Email" />
-            <Input placeholder="Senha" />
+            <InputControl
+              autoCapitalize="none"
+              autoCorrect={false}
+              control={control}
+              name="email"
+              placeholder="Email"
+              keyboardType="email-address"
+            />
+            <InputControl
+              control={control}
+              name="password"
+              placeholder="Senha"
+              autoCorrect={false}
+              secureTextEntry
+            />
 
-            <Button title="Entrar" />
+            <Button title="Entrar" onPress={handleSubmit(handleSignIn)} />
 
             <ForgotPasswordButton>
               <ForgotPasswordTitle>Esqueci minha senha</ForgotPasswordTitle>
