1ebdf848c51ddf082d603f3a44a9afbc8422692a
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-01-27 13:42:35 -0300

    Pagina UserDetails - parte 2

--- src/pages/UserDetails/index.tsx
+++ src/pages/UserDetails/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { useRoute } from '@react-navigation/native';
 import { Container } from './styles';
+import { IUser } from '../../model/user';
+import { api } from '../../services/api';
+
+interface RouteParams {
+  userId: string;
+}
 
 export const UserDetails: React.FunctionComponent = () => {
+  const [userDetails, setUserDetails] = React.useState<IUser>({} as IUser);
   const route = useRoute();
+  const { userId } = route.params as RouteParams;
 
-  console.log(route);
+  React.useEffect(() => {
+    const loadUser = async () => {
+      const response = await api.get(`/users/${userId}`);
+      setUserDetails(response.data);
+    };
+    loadUser();
+  }, [userId]);
 
   return <Container />;
 };
