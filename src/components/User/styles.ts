f01114fc4168ddab8a254312a56d70f1f8ed0b4d
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-01-27 11:08:40 -0300

    Componente User - parte 2

--- src/components/User/styles.ts
+++ src/components/User/styles.ts
@@ -1,19 +1,55 @@
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  width: 100%;
+  height: ${RFValue(100)}px;
+  background-color: ${({ theme }) => theme.colors.gray800};
+  border-radius: 10px;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  margin: 8px 0;
+`;
 
 export const UserDetail = styled.View``;
 
-export const UserNameDetail = styled.View``;
+export const UserNameDetail = styled.View`
+  margin-bottom: 16px;
+`;
 
-export const NameTitle = styled.Text``;
+export const NameTitle = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  color: ${({ theme }) => theme.colors.light};
+  font-size: ${RFValue(8)}px;
+  text-transform: uppercase;
+`;
 
-export const NameData = styled.Text``;
+export const NameData = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  color: ${({ theme }) => theme.colors.primary};
+  font-size: ${RFValue(14)}px;
+`;
 
 export const UserEmailDetail = styled.View``;
 
-export const EmailTitle = styled.Text``;
+export const EmailTitle = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  color: ${({ theme }) => theme.colors.light};
+  font-size: ${RFValue(8)}px;
+  text-transform: uppercase;
+`;
 
-export const EmailData = styled.View``;
+export const EmailData = styled.View`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  color: ${({ theme }) => theme.colors.primary};
+  font-size: ${RFValue(14)}px;
+`;
 
-export const UserAvatar = styled.Image``;
+export const UserAvatar = styled.Image`
+  width: ${RFValue(48)}px;
+  height: ${RFValue(48)}px;
+  border-radius: 10px;
+`;
