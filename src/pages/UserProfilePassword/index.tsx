78efdf19ac4110c61d1c58828c98109c350cbe6f
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-03-28 12:47:52 -0300

    Pagina de atualizacao de senha - parte 1

---
+++ src/pages/UserProfilePassword/index.tsx
@@ -0,0 +1,130 @@
+import React from 'react';
+import {
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from 'react-native';
+import { useForm, FieldValues } from 'react-hook-form';
+import { useNavigation } from '@react-navigation/native';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+import { api } from '../../services/api';
+import { Button } from '../../components/Form/Button';
+import { InputControl } from '../../components/Form/InputControl';
+import {
+  Container,
+  Content,
+  GoBackButton,
+  Header,
+  HeaderTitle,
+  Icon,
+  Title,
+  UserAvatar,
+} from './styles';
+import { useAuth } from '../../context/AuthContext';
+import avatarDefault from '../../assets/avatar02.png';
+
+interface ScreenNavigationProp {
+  goBack: () => void;
+}
+
+interface IFormInputs {
+  [name: string]: any;
+}
+
+const formSchema = yup.object({
+  old_password: yup.string().required('Campo obrigatório.'),
+  password: yup.string().required('Campo obrigatório.'),
+  password_confirmation: yup
+    .string()
+    .required('Campo obrigatório.')
+    .oneOf([yup.ref('password'), null], 'Confirmação incorreta.'),
+});
+
+export const UserProfilePassword: React.FunctionComponent = () => {
+  const { user, updateUser } = useAuth();
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    resolver: yupResolver(formSchema),
+  });
+
+  const { goBack } = useNavigation<ScreenNavigationProp>();
+
+  const handleUpdatePassword = async (form: IFormInputs) => {
+    const data = {
+      old_password: form.old_password,
+      password: form.password,
+      password_confirmation: form.password_confirmation,
+    };
+
+    try {
+      const response = await api.put('profile', data);
+      updateUser(response.data);
+      Alert.alert('Senha atualizada', 'Senha atualizada com sucesso.');
+      goBack();
+    } catch (error) {
+      Alert.alert(
+        'Erro ao atualizar',
+        'Ocorreu um erro ao atualizar a senha. Tente novamente.',
+      );
+    }
+  };
+
+  return (
+    <KeyboardAvoidingView
+      enabled
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+    >
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ flex: 1 }}
+      >
+        <Container>
+          <Header>
+            <GoBackButton onPress={goBack}>
+              <Icon name="chevron-left" />
+            </GoBackButton>
+            <HeaderTitle>Seu perfil</HeaderTitle>
+            <UserAvatar
+              source={
+                user.avatar_url ? { uri: user.avatar_url } : avatarDefault
+              }
+            />
+          </Header>
+          <Content>
+            <Title>Editar dados do perfil</Title>
+            <InputControl
+              autoCapitalize="none"
+              autoCorrect={false}
+              control={control}
+              name="name"
+              placeholder="Nome completo"
+              error={errors.name && errors.name.message}
+            />
+            <InputControl
+              autoCapitalize="none"
+              autoCorrect={false}
+              control={control}
+              name="email"
+              placeholder="Email"
+              keyboardType="email-address"
+              error={errors.email && errors.email.message}
+            />
+
+            <Button
+              title="Salvar alterações"
+              onPress={handleSubmit(handleProfileEdit)}
+              disabled={!!errors.name || !!errors.email}
+            />
+          </Content>
+        </Container>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+};
---
+++ src/pages/UserProfilePassword/styles.ts
@@ -0,0 +1,83 @@
+import styled from 'styled-components/native';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+import { Feather } from '@expo/vector-icons';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.dark};
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFPercentage(16)}px;
+  background-color: ${({ theme }) => theme.colors.secondary};
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  padding: ${RFValue(48)}px ${RFValue(24)}px ${RFValue(8)}px;
+`;
+
+export const GoBackButton = styled.TouchableOpacity`
+  margin-right: ${RFValue(16)}px;
+`;
+
+export const HeaderTitle = styled.Text`
+  color: ${({ theme }) => theme.colors.gray800};
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.bold};
+  font-weight: bold;
+`;
+
+export const UserAvatar = styled.Image`
+  width: ${RFValue(48)}px;
+  height: ${RFValue(48)}px;
+  border-radius: 10px;
+  margin-left: auto;
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  padding: 24px;
+`;
+
+export const Title = styled.Text`
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+  color: ${({ theme }) => theme.colors.light};
+  margin-bottom: 24px;
+`;
+
+export const Logo = styled.Image`
+  width: ${RFValue(160)}px;
+  height: ${RFValue(160)}px;
+  margin-bottom: ${RFValue(64)}px;
+`;
+
+export const Icon = styled(Feather)`
+  font-size: ${RFValue(24)}px;
+  color: ${({ theme }) => theme.colors.gray800};
+`;
+
+export const BackToSignIn = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: ${({ theme }) => theme.colors.gray800};
+  border-top-width: 1px;
+  border-color: ${({ theme }) => theme.colors.black};
+  padding: 16px 0;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const BackToSignInTitle = styled.Text`
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+  color: ${({ theme }) => theme.colors.primary};
+  margin-left: 16px;
+`;
